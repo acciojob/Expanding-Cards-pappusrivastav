d5645dd22f4a7473dabe59b2e454f0512fca4e6f
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-03 17:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function expandCard(panelId) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => {
+        if (panel.id === panelId) {
+            panel.classList.toggle('active');
+            panel.classList.toggle('expanding');
+        } else {
+            panel.classList.remove('active');
+            panel.classList.remove('expanding');
+        }
+    });
+}
